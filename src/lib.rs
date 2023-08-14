9074be815edbe18de9307d707a057befa1c7c583
ag-dragon <h28941+github@gmail.com>
2023-08-13 22:55:36 -0400

    Remove browser support

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,6 @@ use winit::{
 };
 use winit::window::Window;
 
-#[cfg(target_arch="wasm32")]
-use wasm_bindgen::prelude::*;
-
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -176,35 +173,11 @@ impl State {
     }
 }
 
-#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub async fn run() {
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch="wasm32")] {
-            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
-        } else {
-            env_logger::init();
-        }
-    }
+    env_logger::init();
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    #[cfg(target_arch="wasm32")]
-    {
-        use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(450, 400));
-
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                let dst = doc.get_element_by_id("voxel-engine")?;
-                let canvas = web_sys::Element::from(window.canvas());
-                dst.append_child(&canvas).ok()?;
-                Some(())
-            }).expect("Couldn't append canvas to document body.");
-    }
 
     let mut state = State::new(window).await;
 
