38cb258c4e9c51f63a5368867313f793c7d8341b
ag-dragon <h28941+github@gmail.com>
2023-08-20 19:19:09 -0400

    Reset back to window creation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-use voxel_engine::run;
+use winit::{
+    event::{Event, WindowEvent, ElementState, VirtualKeyCode, KeyboardInput},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
-    futures::executor::block_on(run());
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    _ => {}
+                }
+            }
+            Event::MainEventsCleared => {
+            }
+            _ => (),
+        }
+    });
 }
