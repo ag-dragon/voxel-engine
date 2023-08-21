b9bfa38cb891dc20ab6ded7a8076d6bb7a6a51a7
ag-dragon <h28941+github@gmail.com>
2023-08-21 11:03:29 -0400

    Add gpustate struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod gpu_state;
+use gpu_state::GpuState;
 use winit::{
     event::{Event, WindowEvent, ElementState, VirtualKeyCode, KeyboardInput},
     event_loop::{ControlFlow, EventLoop},
@@ -10,12 +12,14 @@ fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
+    let gpu_state = pollster::block_on(GpuState::new(window));
+
     event_loop.run(move |event, _, control_flow| {
         match event {
             Event::WindowEvent {
                 ref event,
                 window_id,
-            } if window_id == window.id() => {
+            } if window_id == gpu_state.window.id() => {
                 match event {
                     WindowEvent::CloseRequested
                     | WindowEvent::KeyboardInput {
