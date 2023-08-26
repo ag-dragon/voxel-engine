794188d6476aeed5a9c84a1a7feaea457df81ba0
ag-dragon <h28941+github@gmail.com>
2023-08-26 16:46:24 -0400

    Revert "Reset back to window creation"
    This reverts commit 38cb258c4e9c51f63a5368867313f793c7d8341b.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,5 @@
-mod gpu_state;
-use gpu_state::GpuState;
-use winit::{
-    event::{Event, WindowEvent, ElementState, VirtualKeyCode, KeyboardInput},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use voxel_engine::run;
 
 fn main() {
-    env_logger::init();
-
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    let gpu_state = pollster::block_on(GpuState::new(window));
-
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == gpu_state.window.id() => {
-                match event {
-                    WindowEvent::CloseRequested
-                    | WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    } => *control_flow = ControlFlow::Exit,
-                    _ => {}
-                }
-            }
-            Event::MainEventsCleared => {
-            }
-            _ => (),
-        }
-    });
+    futures::executor::block_on(run());
 }
