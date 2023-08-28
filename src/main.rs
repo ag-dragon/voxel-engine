7a6630f4f41279384896ca0408f187a10049599f
ag-dragon <h28941+github@gmail.com>
2023-08-27 21:15:30 -0400

    Change vertices to draw cube

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,29 @@ struct Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
+    Vertex { position: [-1.0, 1.0, 1.0], tex_coords: [0.0, 0.0], },
+    Vertex { position: [1.0, 1.0, 1.0], tex_coords: [1.0, 0.0], },
+    Vertex { position: [-1.0, -1.0, 1.0], tex_coords: [0.0, 1.0], },
+    Vertex { position: [1.0, -1.0, 1.0], tex_coords: [1.0, 1.0], },
+    Vertex { position: [-1.0, 1.0, -1.0], tex_coords: [1.0, 0.0], },
+    Vertex { position: [1.0, 1.0, -1.0], tex_coords: [1.0, 1.0], },
+    Vertex { position: [-1.0, -1.0, -1.0], tex_coords: [0.0, 0.0], },
+    Vertex { position: [1.0, -1.0, -1.0], tex_coords: [0.0, 1.0], },
 ];
 
 const INDICES: &[u16] = &[
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
+    3, 1, 2,
+    2, 1, 0,
+    0, 5, 4,
+    0, 1, 5,
+    4, 5, 6,
+    6, 5, 7,
+    2, 6, 7,
+    2, 7, 3,
+    5, 1, 3,
+    5, 3, 7,
+    0, 4, 6,
+    0, 6, 2,
 ];
 
 impl Vertex {
